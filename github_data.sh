dbad3ef61ae84d633d606f98467a6967a3f530aa
mike yang <18604713262@163.com>
2021-11-04 12:41:20 -0400

    Running version

--- github_data.sh
+++ github_data.sh
@@ -3,7 +3,15 @@ githubOutFile='./github_out.json'
 save_p='../checkout'
 # cur_page=1
 per_page=100
-file_count=0
+file_count=5000
+github_u=$1
+github_s=$2
+if [ -z "$github_s" ]; then 
+  echo "Need github id and key!"
+  exit
+else
+  echo "Login for $github_u:"
+fi
 get_repo(){
   jqvars="$(echo "$1" | jq '{branch: .default_branch, name: .full_name}')"
   reponame="$(  echo "${jqvars}" | jq -r '.name' )"
@@ -25,30 +33,33 @@ get_query_len(){
 }
 
 write_page(){
-  _page="$(curl -s -H "Accept: application/vnd.github.v3+json" -X GET https://api.github.com/search/repositories?q=language:Python+is:public&page=$cur_page&per_page=$per_page)"
+  _page="$(curl -u "$github_u:$github_s" -s -H "Accept: application/vnd.github.v3+json" -X GET https://api.github.com/search/repositories?q=language:Python+is:public&page=$cur_page&per_page=$per_page)"
   echo "${_page}"
 }
 
 resolve_file(){
   repofull_path=$2
+  echo $repofull_path
   geturl="$(echo $1 | jq -cr '.[]')"
   for cur_f in ${geturl}; do
     wrepo_name="$(echo "${cur_f}"| jq -r '.name')"
     wrepo_type="$(echo "${cur_f}"| jq -r '.type')"
     wrepo_url="$(echo "${cur_f}"| jq -r '.url')"
     wrepo_path="$(echo "${cur_f}"| jq -r '.path')"
-    # echo "[]get $wrepo_name $wrepo_type;"
+    echo "[]get $wrepo_name $wrepo_type;"
     case $wrepo_type in
       file)
         # echo "[]$wrepo_name is file."
-        file_count=$((file_count+1))
+        
         wrepo_dl="$(echo "${cur_f}"| jq -r '.download_url')"
         case $wrepo_name in
           *.py)
             mkdir -p "$save_p/$repofull_path/${wrepo_path%/*}"
-            curl -s $wrepo_dl > "$save_p/$repofull_path/$wrepo_path"
-            echo "[written] $save_p/$repofull_path/$wrepo_path "
-            echo "[written] $save_p/$repofull_path/$wrepo_path " >> logs
+            curl -u "$github_u:$github_s" -s $wrepo_dl > "$save_p/$repofull_path/$wrepo_path"
+            file_count=$((file_count+1))
+            echo "[written $file_count $cur_page] $save_p/$repofull_path/$wrepo_path "
+            echo "[written $file_count $cur_page] $save_p/$repofull_path/$wrepo_path " >> logs
+	    echo "$wrepo_dl" >> logs
             ;;
           *)
             
@@ -56,14 +67,14 @@ resolve_file(){
         esac
         ;;
       dir)
-        # echo "[]$wrepo_name is folder."
+        echo "[]$wrepo_name is folder."
         case $wrepo_name in
           .git)
             ;;
           .github)
             ;;
             *)
-              wrepo_curl_out="$(curl -s $wrepo_url)" 
+              wrepo_curl_out="$(curl -u "$github_u:$github_s" -s $wrepo_url)" 
               resolve_file "${wrepo_curl_out}" $repofull_path
             ;;
         esac
@@ -80,14 +91,14 @@ write_repo(){
 
   for one_repo in ${cur_page_file}; do
     currepo_name="$(get_repo_n $one_repo )"
-    curl_out="$(curl -s $(get_repo $one_repo ))" 
+    curl_out="$(curl -u "$github_u:$github_s" -s $(get_repo $one_repo ))" 
     resolve_file "${curl_out}" $currepo_name
   done
 
 }
 echo "" > logs
-i=1
-while [ "$i" -le 4 ]; do
+i=3
+while [ "$i" -le 8000 ]; do
     # amixer cset numid=1 "$i%"
 
   echo "Page $i"
