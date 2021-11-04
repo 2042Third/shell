81d07d5bf72b9dc2a5654dbe967a684d28483a23
mike yang <18604713262@163.com>
2021-11-04 11:11:01 -0400

    fixed

--- github_data.sh
+++ github_data.sh
@@ -14,6 +14,12 @@ get_repo(){
   echo "$file_count: $reponame, $repobranch" >> logs
   echo "https://api.github.com/repos/${reponame}/contents?ref=${repobranch}"
 }
+get_repo_n(){
+  # exit
+  jqvars="$(echo "$1" | jq '{branch: .default_branch, name: .full_name}')"
+  reponame="$(  echo "${jqvars}" | jq -r '.name' )"
+  echo "${reponame}"
+}
 
 get_query_len(){
   query_len="$(cat $githubOutFile | jq -r '[.total_count]|.[0] ')"
@@ -24,40 +30,67 @@ write_page(){
   _page="$(curl -s -H "Accept: application/vnd.github.v3+json" -X GET https://api.github.com/search/repositories?q=language:Python+is:public&page=$cur_page&per_page=$per_page)"
   echo "${_page}"
 }
-# resolve(){
 
-# }
+resolve_file(){
+  repofull_path=$2
+  echo $repofull_path
+  # echo $1
+  geturl="$(echo $1 | jq -cr '.[]')"
+  # row="${geturl}"
+  for cur_f in ${geturl}; do
+    wrepo_name="$(echo "${cur_f}"| jq -r '.name')"
+    wrepo_type="$(echo "${cur_f}"| jq -r '.type')"
+    wrepo_url="$(echo "${cur_f}"| jq -r '.url')"
+    wrepo_path="$(echo "${cur_f}"| jq -r '.path')"
+    echo "[]get $wrepo_name $wrepo_type;"
+    case $wrepo_type in
+      file)
+        # echo "[]$wrepo_name is file."
+        file_count=$((file_count+1))
+        case $wrepo_name in
+
+          *.py)
+            echo "making dir $save_p/$repofull_path/$wrepo_path"
+            mkdir -p "$save_p/$repofull_path/$wrepo_path"
+            ;;
+          *)
+            
+            ;;
+        esac
+        ;;
+      dir)
+        # echo "[]$wrepo_name is folder."
+        case $wrepo_name in
+          .git)
+            ;;
+          .github)
+            ;;
+            *)
+              wrepo_curl_out="$(curl -s $wrepo_url)" 
+              echo "recur on $repofull_path/$wrepo_path at $wrepo_url"
+              resolve_file "${wrepo_curl_out}" $repofull_path
+            ;;
+        esac
+        ;;
+      *)echo "[]Skipped $wrepo_type";;
+  
+    esac
+    
+  done
+}
+
+
 
 write_repo(){
   cur_page_file="$(cat  $githubOutFile | jq -c '[.items] | .[0]| .[] | {default_branch: .default_branch, full_name: .full_name}')"
 
   # exit
   for one_repo in ${cur_page_file}; do
-
-    
     echo "$(get_repo $one_repo)"
     echo ""
-    echo ""
-    curl_out="$(curl -s $(get_repo $one_repo))" 
-    geturl="$(echo "${curl_out}"| jq -cr '.[]')"
-    # row="${geturl}"
-    for cur_f in ${geturl}; do
-      wrepo_name="$(echo "${cur_f}"| jq -r '.name')"
-      wrepo_type="$(echo "${cur_f}"| jq -r '.type')"
-      wrepo_url="$(echo "${cur_f}"| jq -r '.url')"
-      case $wrepo_type in
-        file)
-          echo "$wrepo_name is file."
-          ((file_count++))
-          ;;
-        dir)
-          echo "$wrepo_name is folder."
-          ;;
-        *)echo "Skipped $wrepo_type";;
-    
-      esac
-      
-    done
+    currepo_name="$(get_repo_n $one_repo )"
+    curl_out="$(curl -s $(get_repo $one_repo ))" 
+    resolve_file "${curl_out}" $currepo_name
     # echo "${geturl}"
     exit
 
