800b3f832965100b54f75f7523d805b17a115232
mike yang <18604713262@163.com>
2021-11-04 10:11:00 -0400

    added new scripts

--- github_data.sh
+++ github_data.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 githubOutFile='./github_out.json'
+save_p='../checkout'
 # cur_page=1
 per_page=100
+file_count=0
 get_repo(){
-  jqvars="$(cat $githubOutFile | jq '[.items] | .[0] | .[0]|{branch: .default_branch, name: .full_name}')"
+  echo "In repo $1"
+  # exit
+  jqvars="$(echo "$1" | jq '{branch: .default_branch, name: .full_name}')"
   reponame="$(  echo "${jqvars}" | jq -r '.name' )"
   repobranch="$(  echo "${jqvars}" | jq -r '.branch' )"
+
+  echo "$file_count: $reponame, $repobranch" >> logs
   echo "https://api.github.com/repos/${reponame}/contents?ref=${repobranch}"
 }
 
@@ -21,23 +27,50 @@ write_page(){
 # resolve(){
 
 # }
+
 write_repo(){
-  curl_out="$(curl -s $(get_repo))" 
-  geturl="$(echo "${curl_out}"| jq -r '.[]')"
-  row="${geturl}"
-  _jq() {
-   echo ${row}  | jq -r ${1}
-  }
-  echo $(_jq '.name')
-  
+  cur_page_file="$(cat  $githubOutFile | jq -c '[.items] | .[0]| .[] | {default_branch: .default_branch, full_name: .full_name}')"
+
+  # exit
+  for one_repo in ${cur_page_file}; do
+
+    
+    echo "$(get_repo $one_repo)"
+    echo ""
+    echo ""
+    curl_out="$(curl -s $(get_repo $one_repo))" 
+    geturl="$(echo "${curl_out}"| jq -cr '.[]')"
+    # row="${geturl}"
+    for cur_f in ${geturl}; do
+      wrepo_name="$(echo "${cur_f}"| jq -r '.name')"
+      wrepo_type="$(echo "${cur_f}"| jq -r '.type')"
+      wrepo_url="$(echo "${cur_f}"| jq -r '.url')"
+      case $wrepo_type in
+        file)
+          echo "$wrepo_name is file."
+          ((file_count++))
+          ;;
+        dir)
+          echo "$wrepo_name is folder."
+          ;;
+        *)echo "Skipped $wrepo_type";;
+    
+      esac
+      
+    done
+    # echo "${geturl}"
+    exit
+
+  done
 
 }
+echo "" > logs
 i=1
-while [ "$i" -le 8000 ]; do
+while [ "$i" -le 1 ]; do
     # amixer cset numid=1 "$i%"
 
   echo "Page $i"
-  echo "Page $i" >> logs
+  echo "Page $i, $(date)" >> logs
   cur_page=$i
   write_page > github_out.json
   write_repo
@@ -45,13 +78,4 @@ while [ "$i" -le 8000 ]; do
   i=$(( i + 1 ))
 done 
 
-
-# for (( a=1; a<=8; a++ ))
-# do
-#   cur_page=$a
-#   write_page > github_out.json
-#   echo $(get_repo)
-#   echo $(get_query_len)
-#   echo $cur_page
-# done
 
--- test.sh
+++ test.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-geturl="$(cat test| jq -r '.[]')"
-row="${geturl}"
+# geturl="$(cat test| jq -r '.[]')"
+# row="${geturl}"
 _jq() {
  echo ${row}  | jq -r ${1}
 }
-echo $(_jq '.name')
+# echo $(_jq '.name')
+get_repo(){
+  echo "In repo"
+  # jqvars="$(echo "${row}" | jq '{branch: .default_branch, name: .full_name}')"
+  reponame="$(  echo "${row}" | jq -r '.full_name' )"
+  repobranch="$(  echo "${row}" | jq -r '.default_branch' )"
+  echo "https://api.github.com/repos/$reponame/contents?ref=$repobranch"
+}
 
+var1="$(cat  test | jq -c '[.items] | .[0]| .[] | {full_name: .full_name, defualt_branch: .default_branch}')"
+row="${var1}"
+
+# while read a
+# do
+#         echo "$a"
+# done < "${row}"
+
+# while read line
+# do
+#   echo "haha $line"
+# done < <("${row}" |tr ' ' '\n' )
+
+
+for a in ${row}; do
+  echo "hahahha $a"
+done
